53df60ae2f96a00511bda195b7ec7ede20f2980f
marqu3zfabricio <marqu3zfabricio@gmail.com>
2025-12-05 17:01:01 -0600

    avance

---
+++ js/animacion.js
@@ -0,0 +1,14 @@
+
+window.addEventListener('load', () => {
+    // Seleccionamos solo el contenido dentro de <main>
+    const mainContent = document.querySelector('main');
+
+    // Partimos de una posición abajo
+    mainContent.style.transform = 'translateY(70px)';
+    mainContent.style.transition = 'transform 0.7s ease-out';
+
+    // Ejecutamos la animación después de un pequeño delay
+    setTimeout(() => {
+        mainContent.style.transform = 'translateY(0)';
+    }, 100);
+});
