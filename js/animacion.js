b30083dff6515957534dc75a3a3c082fe4dd4efb
marqu3zfabricio <marqu3zfabricio@gmail.com>
2025-12-06 16:56:15 -0600

    cambios

--- js/animacion.js
+++ js/animacion.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
 
     // Partimos de una posición abajo
     mainContent.style.transform = 'translateY(70px)';
-    mainContent.style.transition = 'transform 0.7s ease-out';
+    mainContent.style.transition = 'transform 0.4s ease-out';
 
     // Ejecutamos la animación después de un pequeño delay
     setTimeout(() => {
